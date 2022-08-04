7355840d128b420a5d055f97c6974c920a7e2114
christian thompson <cjthomp2005@outlook.com>
2022-08-04 02:16:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# outfitter
+ outfitter is a powerful weather tool that takes local conditions and creates an outfit for the day
---
+++ android/app/src/main/kotlin/com/example/outfitter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.outfitter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
